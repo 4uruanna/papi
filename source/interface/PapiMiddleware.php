96576209a711ef63cdaed12d6933c8bb69a40746
4uruanna <adrien@villalonga.software>
2025-12-17 13:23:46 +0100

    refactor: middleware implementation with a registration function

---
+++ source/interface/PapiMiddleware.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Papi\abstract;
+
+use Psr\Http\Server\MiddlewareInterface;
+use Slim\App;
+
+interface PapiMiddleware extends MiddlewareInterface
+{
+    public static function register(App $app, array &$middlewares_map): bool;
+}
