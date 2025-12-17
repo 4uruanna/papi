3bc3292dfd572fb773aad79d41d7c8331067129e
4uruanna <adrien@villalonga.software>
2025-12-17 13:24:18 +0100

    refactor: builder

---
+++ source/PapiBuilder.php
@@ -0,0 +1,186 @@
+<?php
+
+namespace Papi;
+
+use DI\Bridge\Slim\Bridge;
+use DI\Container;
+use DI\ContainerBuilder;
+use Papi\enumerator\EventType;
+use Papi\interface\PapiAction;
+use Slim\App;
+
+final class PapiBuilder
+{
+    private array $actions;
+
+    private array $definitions;
+
+    private array $events;
+
+    private array $middlewares;
+
+    private array $modules;
+
+    public function __construct()
+    {
+        $this->actions = [];
+        $this->definitions = [];
+        $this->events = [];
+        $this->middlewares = [];
+        $this->modules = [];
+    }
+
+    public function build(): App|false
+    {
+        $app = false;
+
+        $this->notify(EventType::START, []);
+
+        if ($this->setupModules()) {
+            $container = $this->setupDefinitions();
+
+            $app = Bridge::create($container);
+
+            $this->setupMiddlewares($app);
+
+            $this->setupActions($app);
+
+            $this->notify(EventType::END, ['app' => $app]);
+        }
+
+        return $app;
+    }
+
+    public function addAction(string ...$actions): self
+    {
+        array_push($this->actions, ...$actions);
+        return $this;
+    }
+
+    public function addDefinition(array $definitions): self
+    {
+        $this->definitions = array_merge($this->definitions, $definitions);
+        return $this;
+    }
+
+    public function addEvents(string ...$events): self
+    {
+        array_push($this->events, ...$events);
+        return $this;
+    }
+
+    public function addMiddlewares(string ...$middlewares): self
+    {
+        array_push($this->middlewares, ...$middlewares);
+        return $this;
+    }
+
+    public function addModules(string ...$modules): self
+    {
+        array_push($this->modules, ...$modules);
+        return $this;
+    }
+
+    private function notify(EventType $event_type, array $options): void
+    {
+        foreach ($this->events as $event) {
+            $event($event_type, $options);
+        }
+    }
+
+    private function setupActions(App $app): void
+    {
+        $this->notify(
+            EventType::CONFIGURE_ACTIONS,
+            [
+                'app' => $app,
+                'actions' => $this->actions
+            ]
+        );
+
+        foreach ($this->actions as $pattern => $action) {
+            $interfaces = class_implements($action);
+
+            if (isset($interfaces[PapiAction::class])) {
+                $method = $action::getMethod()->value;
+                $pattern = $action::getPattern();
+                $app->$method($pattern, $action);
+            }
+        }
+    }
+
+    private function setupDefinitions(): Container
+    {
+        $container_builder = new ContainerBuilder();
+
+        $this->notify(
+            EventType::CONFIGURE_DEFINITIONS,
+            [
+                'container_builder' => $container_builder,
+                'definitions' => $this->definitions
+            ]
+        );
+
+        $container_builder->addDefinitions($this->definitions);
+
+        return $container_builder->build();
+    }
+
+    private function setupMiddlewares(App $app): void
+    {
+        $this->notify(
+            EventType::CONFIGURE_MIDDLEWARES,
+            [
+                'app' => $app,
+                'middlewares' => $this->middlewares
+            ]
+        );
+
+        $middlewares_map = [];
+
+        while (count($this->middlewares) !== 0) {
+            foreach ($this->middlewares as $middleware) {
+                if (isset($middlewares_map[$middleware]) === false) {
+                    if ($middleware::register($app, $middlewares_map)) {
+                        $middlewares_map[$middleware] = true;
+                    }
+                }
+            }
+
+            $this->middlewares = array_filter(
+                $this->middlewares,
+                fn($m) => isset($middlewares_map[$m]) === false
+            );
+        }
+    }
+
+    private function setupModules(): bool
+    {
+        $result = true;
+
+        foreach ($this->modules as $module_class) {
+            if (is_subclass_of($module_class, PapiModule::class)) {
+                /**
+                 * @var PapiModule
+                 */
+                $module = new $module_class();
+
+                $prerequisites = $module::getPrerequisites();
+
+                foreach ($prerequisites as $prerequisite) {
+                    if (isset($this->modules[$prerequisite]) === false) {
+                        $result = false;
+                    }
+                }
+
+                $module::configure();
+                $this->addDefinition($module::getDefinitions());
+                $this->addEvents(...$module::getEvents());
+                $this->addAction(...$module::getActions());
+                $this->addMiddlewares(...$module::getMiddlewares());
+            }
+        }
+
+        return $result;
+    }
+}
